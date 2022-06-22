a978e0a728df5ef86a43bbd0cba64954cfdabada
ArchaicSB <106488341+ArchaicSB@users.noreply.github.com>
2022-06-22 12:40:39 -0700

    Update App.js

--- src/App.js
+++ src/App.js
@@ -407,7 +407,7 @@ function App() {
           </s.Container>
         </ResponsiveWrapper>
         <s.SpacerMedium />
-        <s.Container jc={"center"} ai={"center"} style={{ width: "50%" }}>
+        <s.Container jc={"center"} ai={"center"} style={{ width: "20%" }}>
           <s.TextDescription
             style={{
               textAlign: "center",
