50dfab57bf912ca8eda354d63f80553d5c55caf3
ArchaicSB <106488341+ArchaicSB@users.noreply.github.com>
2022-06-23 10:24:25 -0700

    Update App.js

--- src/App.js
+++ src/App.js
@@ -18,9 +18,9 @@ export const StyledButton = styled.button`
   color: var(--secondary-text);
   width: 125px;
   cursor: pointer;
-  box-shadow: 0px 6px 0px -2px #ffd700;
-  -webkit-box-shadow: 0px 6px 0px -2px rgba(250, 250, 250, 0.3);
-  -moz-box-shadow: 0px 6px 0px -2px rgba(250, 250, 250, 0.3);
+  box-shadow: 0px 6px 0px 2px #ffd700;
+  -webkit-box-shadow: 1px 6px 0px 2px #ffd700;
+  -moz-box-shadow: 1px 6px 0px 2px #ffd700;
   :active {
     box-shadow: #ffd700;
     -webkit-box-shadow: #ffd700;
