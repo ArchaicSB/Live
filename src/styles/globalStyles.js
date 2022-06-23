419217b62f6a0f1bd47f68c74f8fcd4f00f1c6b4
ArchaicSB <106488341+ArchaicSB@users.noreply.github.com>
2022-06-22 19:16:26 -0700

    Update globalStyles.js

--- src/styles/globalStyles.js
+++ src/styles/globalStyles.js
@@ -4,7 +4,7 @@ import styled from "styled-components";
 export const Screen = styled.div`
   background-color: var(--primary);
   background-image: ${({ image }) => (image ? `url(${image})` : "none")};
-  background-size: contain;
+  background-size: cover;
   background-position: center;
   width: 100%;
   min-height: 100vh;
