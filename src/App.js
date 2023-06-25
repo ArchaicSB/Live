f57381a45b64253f2f02d636c327c2534cb30813
ArchaicSB <106488341+ArchaicSB@users.noreply.github.com>
2023-06-24 23:39:16 -0700

    Update App.js

--- src/App.js
+++ src/App.js
@@ -198,14 +198,14 @@ function App() {
       <s.Container
         flex={2}
         ai={"center"}
-        style={{ padding: 32, backgroundColor: "var(--primary)" }}
+        style={{ padding: 12, backgroundColor: "var(--primary)" }}
         image={CONFIG.SHOW_BACKGROUND ? "/config/images/bg.png" : true}
       >
         <a href={CONFIG.MARKETPLACE_LINK2}>
           <StyledLogo alt={"logo"} src={"/config/images/logo.png"} />
         </a>
         <s.SpacerSmall />
-        <ResponsiveWrapper flex={1} style={{ padding: 32 }} test>
+        <ResponsiveWrapper flex={1} style={{ padding: 12 }} test>
           <s.Container flex={1} jc={"center"} ai={"center"}>
             <StyledImg alt={"example"} src={"/config/images/example.gif"} />
           </s.Container>
@@ -216,8 +216,8 @@ function App() {
             ai={"center"}
             style={{
               backgroundColor: "var(--primary)",
-              padding: 32,
-              borderRadius: 32,
+              padding: 12,
+              borderRadius: 12,
               border: "12px solid var(--secondary)",
               boxShadow: "0px 3px 9px 2px rgba(0,0,0,0.7)",
             }}
