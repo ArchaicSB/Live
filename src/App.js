5f66e293724ec9b9e56b4ab83407bce0257b9fe2
ArchaicSB <106488341+ArchaicSB@users.noreply.github.com>
2023-06-25 19:18:48 -0700

    Update App.js

--- src/App.js
+++ src/App.js
@@ -398,7 +398,7 @@ function App() {
             <s.SpacerMedium />
           </s.Container>
           <s.SpacerLarge />
-          <s.Container flex={1} jc={"center"} ai={"center"}>
+          <s.Container flex={2} jc={"center"} ai={"center"}>
             <StyledImg
               alt={"example"}
               src={"/config/images/example.gif"}
