5e086c177496b735726a74e2c9780c13cf4955fb
ArchaicSB <106488341+ArchaicSB@users.noreply.github.com>
2023-08-08 11:04:10 -0700

    Update App.js

--- src/App.js
+++ src/App.js
@@ -415,7 +415,7 @@ const getData = async () => {
           <s.TextDescription
             style={{
               textAlign: "center",
-              color: "var(--primary-text)",
+              color: "var(--secondary-text)",
             }}
           >
             Please make sure you are connected to the right network (
@@ -426,7 +426,7 @@ const getData = async () => {
           <s.TextDescription
             style={{
               textAlign: "center",
-              color: "var(--primary-text)",
+              color: "var(--secondary-text)",
             }}
           >
             We have set the gas limit to {CONFIG.GAS_LIMIT} for the contract to
