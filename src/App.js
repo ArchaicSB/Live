793f67af5e7d55d3573f0041afe977bfcdd2bf96
ArchaicSB <106488341+ArchaicSB@users.noreply.github.com>
2023-08-09 11:48:26 -0700

    Update App.js

--- src/App.js
+++ src/App.js
@@ -18,13 +18,13 @@ export const StyledButton = styled.button`
   color: var(--secondary-text);
   width: 125px;
   cursor: pointer;
-  box-shadow: 0px 6px 0px -2px #0B4008;
-  -webkit-box-shadow: 1px 6px 0px -2px #0B4008;
-  -moz-box-shadow: 1px 6px 0px -2px #0B4008;
+  box-shadow: 0px 6px 0px -2px #000000;
+  -webkit-box-shadow: 1px 6px 0px -2px #000000;
+  -moz-box-shadow: 1px 6px 0px -2px #000000;
   :active {
-    box-shadow: #0B4008;
-    -webkit-box-shadow: #0B4008;
-    -moz-box-shadow: #0B4008;
+    box-shadow: #000000;
+    -webkit-box-shadow: #000000;
+    -moz-box-shadow: #000000;
   }
 `;
 
@@ -47,9 +47,9 @@ export const StyledRoundButton = styled.button`
   -webkit-box-shadow: 0px 4px 0px -2px rgba(250, 250, 250, 0.3);
   -moz-box-shadow: 0px 4px 0px -2px rgba(250, 250, 250, 0.3);
   :active {
-    box-shadow: #0B4008;
-    -webkit-box-shadow: #0B4008;
-    -moz-box-shadow: #0B4008;
+    box-shadow: #000000;
+    -webkit-box-shadow: #000000;
+    -moz-box-shadow: #000000;
   }
 `;
 
