8d483d28e8ce9ffc44cc41f22e8ecea599faf579
ArchaicSB <106488341+ArchaicSB@users.noreply.github.com>
2023-08-14 13:06:59 -0700

    Update App.js

--- src/App.js
+++ src/App.js
@@ -512,8 +512,10 @@ const getData = async () => {
         boxShadow: "0px 3px 9px 2px rgba(0,0,0,0.9)",
       }}>
         <s.SpacerSmall />
+        <div>
         <StyledImg alt={"trash"} src={"/config/images/trash.png"} style={{borderRadius: "10px"}} />
         <StyledImg alt={"garbagepatch"} src={"/config/images/garbagepatch.png"} style={{borderRadius: "10px"}} />
+        </div>
       </s.Container>
     </s.Screen>
   );
