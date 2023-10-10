0ed03cf64b91e90b70bbc54732723335e3c690d9
ArchaicSB <106488341+ArchaicSB@users.noreply.github.com>
2023-10-09 18:14:38 -0700

    Update App.js

--- src/App.js
+++ src/App.js
@@ -218,6 +218,8 @@ function App() {
           color: "#fff",
           fontWeight: "bold",
           fontSize: "33px",
+          margin: "8px",
+          padding: "8px",
         }}>
         Home of the Archaic Shell Babies
       </s.TextTitle>
