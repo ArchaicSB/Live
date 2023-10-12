98668794df28b0014393a622c69ca31c05133053
ArchaicSB <106488341+ArchaicSB@users.noreply.github.com>
2023-10-11 20:50:25 -0700

    edit header color

--- src/App.js
+++ src/App.js
@@ -210,7 +210,7 @@ function App() {
       <s.Container
       style={{
         padding: 8,
-        background: "linear-gradient(to top right, #FFE5B4, #7DF9FF, #FFE5B4)",
+        background: "linear-gradient(to top right, #eee, #fff, #000)",
         width: "device-width"}}
       >
       <s.TextTitle
