16444cebd79561722cfe8dd902f340c8fee6ef94
ArchaicSB <106488341+ArchaicSB@users.noreply.github.com>
2023-10-12 18:16:10 -0700

    edit the navbar

--- src/components/navbar.jsx
+++ src/components/navbar.jsx
@@ -1,11 +1,17 @@
 import Reac from 'react';
 import styled from "styled-components";
+import * as s from "./styles/globalStyles";
+
 
 const Navbar = () => {
     return (
-        <s.Container>
-            <TextTitle>
-                Navbar
+        <s.Container width="device-width"
+        height="40px"
+        background="linear-gradient(to left bottom, #2E3192, #1BFFFF)"
+        style={{border: "4px solid #000"}}
+        >
+            <TextTitle style={{textAlign: "left", padding:"4px 4px", color: "#fff"}}>
+                Archaic Shell Babies
             </TextTitle>
         </s.Container>
     )
