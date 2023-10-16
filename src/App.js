3d58212bc2ea328f29b6d2b7aa3e9b089e9d9976
ArchaicSB <106488341+ArchaicSB@users.noreply.github.com>
2023-10-16 13:33:19 -0700

    testing jc = space between. if not i will try inline block

--- src/App.js
+++ src/App.js
@@ -24,7 +24,7 @@ const Navbar = styled.div`
   background-color: #357960;
   padding: 10px 0;
   display: flex;
-  justify-content: center;
+  justify-content: space-between;
   position: sticky;
   top: 0;
   z-index: 100;
