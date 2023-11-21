c822c3d0f75ae5d5ea7e345de7b35730bab6a864
ArchaicSB <106488341+ArchaicSB@users.noreply.github.com>
2023-11-21 14:10:46 -0800

    changing onClick js code

--- src/App.js
+++ src/App.js
@@ -265,7 +265,7 @@ function App() {
   <s.Screen image={CONFIG.SHOW_BACKGROUND ? "/config/images/bg.png" : true}>
       <Navbar id="navbar" style={{background: "linear-gradient(90deg, #FFE5B4, #7DF9FF, #FFE5B4)"}}>
         <s.TextTitle onClick={(e) => {
-                window.open("https://www.archaicshellbabies.com");
+                window.open("https://www.archaicshellbabies.com", "_top");
               }}
           style={{
             textAlign: "left",
